aab06fe3ad7b28a1fd8402ed01a8f23655265826
Victor Tran <vicr12345@gmail.com>
2018-10-27 10:54:53 +1100

    Fix Contemporary not working on macOS

--- travis.sh
+++ travis.sh
@@ -57,7 +57,7 @@ if [ $STAGE = "script" ]; then
     macdeployqt thePhoto.app
     echo "[TRAVIS] Deploying Contemporary"
     cp ../contemporary-theme/libContemporary.dylib thePhoto.app/Contents/Plugins/styles/
-    install_name_tool -change libthe-libs.1.dylib @executable_path/../Libraries/libthe-libs.1.dylib thePhoto.app/Contents/Plugins/styles/libContemporary.dylib
+    install_name_tool -change libthe-libs.1.dylib @executable_path/../../Libraries/libthe-libs.1.dylib thePhoto.app/Contents/Plugins/styles/libContemporary.dylib
     install_name_tool -change @rpath/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtSvg.framework/Versions/5/QtWidgets thePhoto.app/Contents/Plugins/styles/libContemporary.dylib
     install_name_tool -change @rpath/QtWidgets.framework/Versions/5/QtGui @executable_path/../Frameworks/QtSvg.framework/Versions/5/QtGui thePhoto.app/Contents/Plugins/styles/libContemporary.dylib
     install_name_tool -change @rpath/QtWidgets.framework/Versions/5/QtCore @executable_path/../Frameworks/QtSvg.framework/Versions/5/QtCore thePhoto.app/Contents/Plugins/styles/libContemporary.dylib
