78e57b992602abee820ea323fef80e3877002c3b
Victor Tran <vicr12345@gmail.com>
2018-10-29 21:07:43 +1100

    Travis build

--- travis.sh
+++ travis.sh
@@ -1,5 +1,11 @@
 if [ $STAGE = "beforeinstall" ]; then
   yes | sdkmanager "platforms;android-27";
 elif [ $STAGE = "script" ]; then
-  ./gradlew build connectedCheck;
+  ./gradlew assembleRelease;
+  cp app/build/outputs/apk/release/app-release.apk ~/thePhoto-android.apk
+elif [ $STAGE = "after_success" ]; then
+  echo "[TRAVIS] Publishing APK"
+  cd
+  wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
+  bash upload.sh thePhoto-android.apk
 fi
