bedb82638825963d03e80006154ee74837b672e4
Victor Tran <vicr12345@gmail.com>
2018-10-29 22:12:28 +1100

    Travis push to play store

--- travis.sh
+++ travis.sh
@@ -1,5 +1,7 @@
-if [ $STAGE = "beforeinstall" ]; then
-  yes | sdkmanager "platforms;android-27";
+if [ $STAGE = "before_install" ]; then
+  openssl aes-256-cbc -K $encrypted_ed87c803ec54_key -iv $encrypted_ed87c803ec54_iv -in secrets.tar.enc -out secrets.tar -d
+  tar -xvf secrets.tar
+  cp google-play.json ~/
 elif [ $STAGE = "script" ]; then
   ./gradlew assembleRelease;
   cp app/build/outputs/apk/release/app-release.apk ~/thePhoto-android.apk
@@ -8,4 +10,9 @@ elif [ $STAGE = "after_success" ]; then
   cd
   wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
   bash upload.sh thePhoto-android.apk
+  if [ $TRAVIS_BRANCH = "android-blueprint" ]; then
+    echo "[TRAVIS] Pushing to Google Play"
+    gem install fastlane
+    fastlane supply --apk thePhoto-android.apk --track beta --json_key google-play.json --package_name com.vicr123.thephotoevent
+  fi
 fi
