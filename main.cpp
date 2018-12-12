f3e4235f0ece379615c8f782929612f6bb42f0f9
Victor Tran <vicr12345@gmail.com>
2018-12-12 23:02:13 +1100

    Use tApplication

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-#include <QApplication>
+#include <tapplication.h>
 #include <QTranslator>
 
 #ifdef Q_OS_MAC
@@ -8,19 +8,11 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    tApplication a(argc, argv);
 
     a.setOrganizationName("theSuite");
     a.setOrganizationDomain("");
     a.setApplicationName("thePhoto");
-
-    QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "Services");
-    QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "Hide %1");
-    QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "Hide Others");
-    QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "Show All");
-    QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "Preferences...");
-    QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "About %1");
-    QT_TRANSLATE_NOOP("MAC_APPLICATION_MENU", "Quit %1");
 
     QTranslator localTranslator;
 #ifdef Q_OS_MAC
